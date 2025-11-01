aa81610f25b5ec057c8fad3918b3e369173bd725
sollniss <sollniss@web.de>
2025-11-01 22:31:22 +0900

    wip

--- configuration.nix
+++ configuration.nix
@@ -86,7 +86,7 @@
   };
 
   # Install firefox.
-  programs.firefox.enable = true;
+  #programs.firefox.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
--- home.nix
+++ home.nix
@@ -5,12 +5,27 @@
     home.homeDirectory = "/home/sollniss";
     home.stateVersion = "25.05";
 
+    # this makes only firefox dark
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+
+    #gtk = {
+    #    enable = true;
+    #    theme = {
+    #        name = "Breeze-Dark";
+    #        package = pkgs.libsForQt5.breeze-gtk;
+    #    };
+    #};
+
     home.packages = with pkgs; [
         gopls
     ];
 
     imports = [
-        .progs/firefox.nix
+        ./progs/firefox.nix
     ];
 
     programs.bash = {
