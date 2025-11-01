0e7c13157e531cd0ed31f671b3b86852aca734af
sollniss <sollniss@web.de>
2025-11-01 17:45:28 +0900

    home manager

--- flake.nix
+++ flake.nix
@@ -3,11 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      # keep home-manager from pulling it's own versions of nixpkgs
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.sollniss = import ./home.nix;
+            backupFileExtension = "backup";
+          };
+        }
+      ];
     };
   };
 }
---
+++ home.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+
+{
+    home.username = "sollniss";
+    home.homeDirectroy = "/home/sollniss";
+    home.stateVersion = "25.05";
+    #programs.bash = {
+    #    enable = true;
+    #};
+}
