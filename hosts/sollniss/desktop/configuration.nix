0c28c0c78eab8f0ddbafdd39112e56bd366a0d0d
sollniss <sollniss@web.de>
2025-12-22 00:18:42 +0900

    update flakes, add nh

--- hosts/sollniss/desktop/configuration.nix
+++ hosts/sollniss/desktop/configuration.nix
@@ -38,6 +38,13 @@ in {
     bash
   ];
 
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/${vars.username}/nix-config"; # Sets NH_OS_FLAKE variable
+  };
+
   users.users.${vars.username} = {
     isNormalUser = true;
     description = vars.username;
