6f1a898ad96e799bb8c116aa6f62362abca933c2
sollniss <sollniss@web.de>
2025-12-22 01:58:45 +0900

    fish cleanup

--- modules/home-manager/programs/fish.nix
+++ modules/home-manager/programs/fish.nix
@@ -1,28 +1,31 @@
-{
-  ...
-}:
-{
+{...}: {
   programs.fish = {
     enable = true;
+
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
+    '';
 
-
-      function nixdiff --argument arg
+    functions = {
+      nixdiff = {
+        argumentNames = ["arg"];
+        body = ''
           if test -z "$arg"
               set arg 0
           else if [ "$arg" = "-a" ]
               nix profile diff-closures --profile /nix/var/nix/profiles/system
               return 0
           end
-          
+
           set tail_n_val (math $arg - 2)
 
           set versions (command ls -dv /nix/var/nix/profiles/system-*-link | tail $tail_n_val | head -2)
           echo $versions
           nix store diff-closures $versions
-      end
-    '';
+        '';
+      };
+    };
+
     shellAbbrs = {
       sys = "nix-shell -p fastfetch --run fastfetch";
     };
