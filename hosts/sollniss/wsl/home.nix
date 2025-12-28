75ee1accfef04de4fe5df8ccf8f4a912ad517925
sollniss <sollniss@web.de>
2025-12-28 22:19:43 +0900

    zed config

--- hosts/sollniss/wsl/home.nix
+++ hosts/sollniss/wsl/home.nix
@@ -28,14 +28,70 @@ in {
   home.packages = with pkgs; [
     # development
     gopls # Go LSP
+    gofumpt
     gcc
+    delve
     #nil # Nix LSP
     nixd
+    alejandra
   ];
 
   programs.zed-editor = {
     enable = true;
     installRemoteServer = true;
+
+    extensions = [ "nix" "make" ];
+    #mutableUserSettings = false;
+    userSettings = {
+      hour_format = "hour24";
+      format_on_save = "on";
+      ui_font_family = "Noto Sans";
+      buffer_font_family = "JetBrains Mono";
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+      };
+      lsp = {
+        gopls = {
+          binary = {
+            path_lookup = true;
+          };
+        };
+        nixd = {
+          binary = {
+            path_lookup = true;
+          };
+          #initialization_options = {
+          #  formatting = {
+          #    command = "alejandra";
+          #    arguments = ["--quiet" "--"];
+          #  };
+          #};
+        };
+      };
+      languages = {
+        Nix = {
+          language_servers = [ "nixd" ];
+          tab_size = 2;
+          formatter = {
+            external = {
+              command = "alejandra";
+              arguments = ["--quiet" "--"];
+            };
+          };
+        };
+        Go = {
+          language_servers = [ "gopls" ];
+          tab_size = 4;
+          format_on_save = "on";
+          formatter = {
+            external = {
+              command = "gofumpt";
+            };
+          };
+        };
+      };
+    };
   };
 
   # Extra programs.
