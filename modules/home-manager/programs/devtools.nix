672bcf47534b3bc1e2814f46526e4fa96682b8d3
sollniss <sollniss@web.de>
2026-06-14 22:39:41 +0900

    set meld as merge editor in jj

--- modules/home-manager/programs/devtools.nix
+++ modules/home-manager/programs/devtools.nix
@@ -28,12 +28,22 @@
   programs.jujutsu = {
     enable = true;
     settings = {
-      ui.diff-formatter = [
-        "difft"
-        "--color=always"
-        "$left"
-        "$right"
-      ];
+      ui = {
+        diff-formatter = [
+          "difft"
+          "--color=always"
+          "$left"
+          "$right"
+        ];
+        merge-editor = lib.mkIf config.prefs.profile.graphical.enable [
+          "meld"
+          "$left"
+          "$base"
+          "$right"
+          "-o"
+          "$output"
+        ];
+      };
     };
   };
 
