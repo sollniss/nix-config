65bdb19d128a4b4c52e39e7ac80d8d5ab812ee2f
sollniss <sollniss@web.de>
2026-06-14 23:33:49 +0900

    add mergiraf, pin difftastic and meld in jj

--- modules/home-manager/programs/devtools.nix
+++ modules/home-manager/programs/devtools.nix
@@ -25,24 +25,34 @@
     git.enable = true;
   };
 
+  programs.mergiraf = {
+    enable = true;
+    enableGitIntegration = true;
+    enableJujutsuIntegration = true;
+  };
+
   programs.jujutsu = {
     enable = true;
     settings = {
       ui = {
         diff-formatter = [
-          "difft"
+          (lib.getExe config.programs.difftastic.package)
           "--color=always"
           "$left"
           "$right"
         ];
-        merge-editor = lib.mkIf config.prefs.profile.graphical.enable [
-          "meld"
-          "$left"
-          "$base"
-          "$right"
-          "-o"
-          "$output"
-        ];
+        # On graphical hosts override mergiraf (set by programs.mergiraf) with
+        # the interactive meld GUI; headless hosts keep mergiraf.
+        merge-editor = lib.mkIf config.prefs.profile.graphical.enable (
+          lib.mkForce [
+            (lib.getExe pkgs.meld)
+            "$left"
+            "$base"
+            "$right"
+            "-o"
+            "$output"
+          ]
+        );
       };
     };
   };
