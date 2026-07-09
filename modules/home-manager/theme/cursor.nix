ca0deb8466fbdb5f18804728c4b9538361a4e8ee
sollniss <sollniss@web.de>
2026-07-09 21:48:45 +0900

    update flakes

--- modules/home-manager/theme/cursor.nix
+++ modules/home-manager/theme/cursor.nix
@@ -6,6 +6,7 @@
 {
   config = lib.mkIf config.prefs.profile.graphical.enable {
     home.pointerCursor = {
+      enable = true;
       size = 24;
       gtk.enable = true;
       x11.enable = true;
