9dc56c3cfa1191a0fec3bf6868a8e341d2d7fe40
sollniss <sollniss@web.de>
2026-07-15 00:12:51 +0900

    disable syncthing relays and nat

--- modules/home-manager/services/syncthing.nix
+++ modules/home-manager/services/syncthing.nix
@@ -6,6 +6,9 @@
       options = {
         globalAnnounceEnabled = false;
         localAnnounceEnabled = true;
+        relaysEnabled = false;
+        # Disable UPnP/NAT-PMP mapping
+        natEnabled = false;
         # Whether the user has accepted to submit anonymous usage data.
         # The default, 0, mean the user has not made a choice, and Syncthing will ask at some point in the future.
         # "-1" means no, a number above zero means that that version of usage reporting has been accepted.
